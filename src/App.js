4651396aa2d01b11327caa55a0425a6e25a942cb
JaimeRomero <jaime_im@outlook.com>
2022-08-03 21:13:32 -0500

    Se importo el componente Display al archivo App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import BarraNav from "./componentes/barraNav";
-
+import Display from "./componentes/Display";
 
 function App() {
   return (
     <div className="App">
         <div className="contenedor-principal">
            <BarraNav />
+           <Display />
         </div>
     </div>
   );
