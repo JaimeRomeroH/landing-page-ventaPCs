63e41343322bdc372598c6dace5af0440db33adb
JaimeRomero <jaime_im@outlook.com>
2022-08-04 19:44:45 -0500

    Se creo el componente Informacion.js

---
+++ src/componentes/Informacion.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../estilos/Informacion.css"
+
+function Informacion(props) {
+     return(
+        <div className="info-contenedor">
+             <h2 className="info-titulo">{props.titulo}</h2>
+             <p className="info-texto">{props.texto}</p>  
+        </div>
+     );
+}
+
+export default Informacion;
