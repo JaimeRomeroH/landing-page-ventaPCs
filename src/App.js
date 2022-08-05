98a17b6357060a895c579af7be5a0bd9cabd62d9
JaimeRomero <jaime_im@outlook.com>
2022-08-04 19:46:16 -0500

    Se importo el componente Informacion aÃl archivo App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import BarraNav from "./componentes/barraNav";
 import Display from "./componentes/Display";
+import Informacion from "./componentes/Informacion";
 
 function App() {
   return (
@@ -8,6 +9,12 @@ function App() {
         <div className="contenedor-principal">
            <BarraNav />
            <Display />
+           <Informacion
+            titulo="¿Quienes somos?" 
+            texto= "Somos una empresa con 10 años de experiencia en mantenimieto y venta de equipo de computo"/>
+           <Informacion
+            titulo="¿Que hacemos?" 
+            texto= "Brindamos servicios de reparacion y venta de equipos de computo principalmente a las pequeñas y medianas empresas"/>
         </div>
     </div>
   );
