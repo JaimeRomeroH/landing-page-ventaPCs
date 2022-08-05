93269ab343a3aa9509bc8a056b5862a05e75ce55
JaimeRomero <jaime_im@outlook.com>
2022-08-04 19:58:00 -0500

    Se agrego un nuevo contenedor a la estructura del componente Informacion con la clase info-contenedor-principal

--- src/componentes/Informacion.js
+++ src/componentes/Informacion.js
@@ -3,10 +3,12 @@ import "../estilos/Informacion.css"
 
 function Informacion(props) {
      return(
-        <div className="info-contenedor">
+       <div className="info-contenedor-principal">
+          <div className="info-contenedor">
              <h2 className="info-titulo">{props.titulo}</h2>
              <p className="info-texto">{props.texto}</p>  
-        </div>
+          </div>
+        </div>  
      );
 }
 
